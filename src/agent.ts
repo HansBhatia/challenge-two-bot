52fe6f6ee5dcfa08f7f9e19b32d5fcef84f6b07f
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-05-15 17:46:28 -0400

    refac: add amount to finding
    chore: wrap handleTransaction

--- src/agent.ts
+++ src/agent.ts
@@ -1,70 +1,39 @@
-import {
-  Finding,
-  FindingSeverity,
-  FindingType,
-  HandleTransaction,
-  TransactionEvent,
-} from "forta-agent";
+import { Finding, FindingSeverity, FindingType, HandleTransaction, TransactionEvent } from "forta-agent";
 
 export const UNI_SWAP_CALLS = [
   `event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)`,
 ];
-export const UNI_SWAP_ROUTER_ADDRESS =
-  "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-let findingsCount = 0;
+export const UNI_SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+const provideHandleTransaction: () => HandleTransaction = () => {
+  const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+    // filter the transaction logs for Tether transfer events
+    const uniSwapFunctionCalls = txEvent.filterLog(UNI_SWAP_CALLS);
 
-  console.log(txEvent);
+    uniSwapFunctionCalls.forEach((swapFunctionCall) => {
+      // extract transfer event arguments
+      const { recipient, sender, amount0, amount1 } = swapFunctionCall.args;
+      // push to findins
+      findings.push(
+        Finding.fromObject({
+          name: "Uniswap swap detected!",
+          description: `Someone made a swap on the uniswap platform.`,
+          alertId: "FORTA-uniswapSwapAlert",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: { to: recipient, from: sender, fromTokenAmount: amount0, toTokenAmount: amount1 },
+        })
+      );
+    });
 
-  // filter the transaction logs for Tether transfer events
-  const uniSwapFunctionCalls = txEvent.filterLog(UNI_SWAP_CALLS);
+    return findings;
+  };
 
-  uniSwapFunctionCalls.forEach((swapFunctionCall) => {
-    // extract transfer event arguments
-    const { recipient, sender } = swapFunctionCall.args;
-    // push to findins
-    findings.push(
-      Finding.fromObject({
-        name: "Uniswap swap detected!",
-        description: `Someone made a swap on the uniswap platform.`,
-        alertId: "FORTA-uniswapSwapAlert",
-        severity: FindingSeverity.Info,
-        type: FindingType.Info,
-        metadata: { to: recipient, from: sender },
-      })
-    );
-    findingsCount++;
-  });
-
-  return findings;
+  return handleTransaction;
 };
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
-
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
-
 export default {
-  // initialize,
-  handleTransaction,
-  // handleBlock,
-  // handleAlert
+  handleTransaction: provideHandleTransaction(),
 };
